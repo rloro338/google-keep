20ec296e4f8fb3a8685fccc0d10f73d67edc945f
Ricardo Loro <ricardo.loroduran@soprasteria.com>
2018-04-11 09:36:22 +0200

    feat: hidden nav menu clicking on shadow div

--- functions.js
+++ functions.js
@@ -1,7 +1,31 @@
 function showmenu(){
 
-  var navigation = document.getElementById("navigation");
-  navigation.classList.toggle("navigation-show");
- 
+  var body = document.getElementsByTagName("body")[0];
+  body.classList.toggle("body-modificado");
   
+  var navigation = document.getElementsByClassName("navigation")[0];
+  navigation.classList.toggle("navigation-show");
+  navigation.style.zIndex=1;
+
+  var div = document.getElementsByClassName("div-empty")[0];
+  div.style.backgroundColor="#454444b7";
+  div.style.zIndex = 2;
+
+  console.log(div);
+  
+}
+
+function divhidden(){
+
+  var body = document.getElementsByTagName("body")[0];
+  body.classList.remove("body-modificado");
+
+  var navigation = document.getElementsByClassName("navigation")[0];
+  navigation.classList.remove("navigation-show");
+
+  var div = document.getElementsByClassName("div-empty")[0];
+  div.style.backgroundColor="#454444b7";
+  div.style.zIndex = -999;
+  
+
 }
