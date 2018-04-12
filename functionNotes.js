88c440c390c5f7590fd027d3f264daa0a9720261
Ricardo Loro <ricardo.loroduran@soprasteria.com>
2018-04-12 17:04:38 +0200

    fix: add select note background color functionality.

--- functionNotes.js
+++ functionNotes.js
@@ -2,6 +2,7 @@
 var newNoteButton = document.getElementById("newNoteButton");
 var content = document.getElementsByClassName("content")[0];
 var input = document.getElementsByClassName("inputText")[0];
+var radioColor = document.getElementsByClassName("radio-color");
 
 
 newNoteButton.addEventListener("click",makeNewNote);
@@ -14,19 +15,37 @@ function checkInput(){
   else{
     newNoteButton.disabled=false;
     }
-}  
+}
+
+function getColor(){
+  var color;
+  for(i=0;i<radioColor.length;i++){
+    if(radioColor[i].checked){
+      color = radioColor[i].value;
+    }
+  }
+  return color;
+}
+
 
 function makeNewNote(){
+    var color = getColor();
     var div = document.createElement("div");
     var p = document.createElement("p");
     var noteContent = document.createTextNode(input.value);
     p.appendChild(noteContent);
     div.appendChild(p);
+    div.style.backgroundColor = color;
     div.classList.add("content-notes");
     content.appendChild(div);
     input.value="";
     newNoteButton.disabled=true;
-    console.log(div);
+
+   
+ 
+    
+   
+
 }
 
 
