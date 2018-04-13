55c70cff2f1d8f75d510428d8d6ab2302845645b
Ricardo Loro <ricardo.loroduran@soprasteria.com>
2018-04-13 12:05:56 +0200

    feat: show/hide color note selection.

--- functionNotes.js
+++ functionNotes.js
@@ -1,21 +1,41 @@
 
-var newNoteButton = document.getElementById("newNoteButton");
+var newNoteButton = document.getElementsByClassName("new-note-button")[0];
 var content = document.getElementsByClassName("content")[0];
 var input = document.getElementsByClassName("inputText")[0];
 var radioColor = document.getElementsByClassName("radio-color");
+var selectColor = document.getElementsByClassName("select-color-hidden")[0];
+var searcher = document.getElementsByClassName("searcher-item")[0];
 
-
-newNoteButton.addEventListener("click",makeNewNote);
+newNoteButton.addEventListener("click",makeNewNote,hideColors);
+content.addEventListener("click",hideColors);
+input.addEventListener("click",showColors);
 input.addEventListener("input", checkInput);
 
+
 function checkInput(){
+  var trigger = false;
   if(input.value==""){
-    newNoteButton.disabled=true;
-    }
+    newNoteButton.disabled=true; 
+  }
   else{
     newNoteButton.disabled=false;
-    }
+  }
 }
+
+function showColors(e){
+  selectColor.classList.remove("select-color-hidden");
+  selectColor.classList.add("select-color");
+  e.stopPropagation();
+}
+
+function hideColors(e){
+  var targetClick = e.target.classList.value;
+  if((targetClick == "content")||targetClick==("new-note-button")){
+    selectColor.classList.remove("select-color");
+    selectColor.classList.add("select-color-hidden");
+  }
+  console.log(e.target.classList.value);
+  }
 
 function getColor(){
   var color;
@@ -26,7 +46,6 @@ function getColor(){
   }
   return color;
 }
-
 
 function makeNewNote(){
     var color = getColor();
@@ -39,13 +58,7 @@ function makeNewNote(){
     div.classList.add("content-notes");
     content.appendChild(div);
     input.value="";
-    newNoteButton.disabled=true;
-
-   
- 
-    
-   
-
+    newNoteButton.disabled=true;    
 }
 
 
